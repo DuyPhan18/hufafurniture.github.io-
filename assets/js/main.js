80af8e4e37bb23904499e22aaf6351590a52de90
DuyPhan18 <Duyptr1998@gmail.com>
2022-07-02 02:44:20 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,37 @@
-    const $ = document.querySelector.bind(document);
-    const $$ = document.querySelectorAll.bind(document);
-    /*-------------NAV-----------*/
    
+    /*-------------Carousel-----------*/
+   $(document).ready(function () {
+    $('.next-button').on('click', function () {
+        var currentBanner = $('.sld-active');
+        var nextBanner = currentBanner.next();
+        if(nextBanner.length){
+            currentBanner.removeClass('sld-active').css('z-index', -10);
+            nextBanner.addClass('sld-active').css('z-index', 10);
+        }
+        })
+    $('.prev-button').on('click', function () {
+        var currentBanner = $('.sld-active');
+        var prevBanner = currentBanner.prev();
+
+        if(prevBanner.length){
+            currentBanner.removeClass('sld-active').css('z-index', -10);
+            prevBanner.addClass('sld-active').css('z-index', 10);
+        }
+        })
+   })
     
     /*---------TAB-----------------*/
-    const tabItem = $$('.tab_item');
-    const tabPane = $$('.tab_pane');
+    const one = document.querySelector.bind(document);
+    const all = document.querySelectorAll.bind(document);
+
+    const tabItem = all('.tab_item');
+    const tabPane = all('.tab_pane');
 
     tabItem.forEach((tab, index) => {   
         const pane = tabPane[index];
         tab.onclick = function () {
-            $('.tab_item.active-tab').classList.remove('active-tab');
-            $('.tab_pane.active-tab').classList.remove('active-tab');
+            one('.tab_item.active-tab').classList.remove('active-tab');
+            one('.tab_pane.active-tab').classList.remove('active-tab');
 
             this.classList.add('active-tab');
             pane.classList.add('active-tab');
